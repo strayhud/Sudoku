0d5a43f58c1ed7a54c7d09efa7d931d574ae916d
Steve Hudson <strayhud@gmail.com>
2010-09-14 12:03:49 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Suduko::Application.config.secret_token = 'a8b3ab9de3366c271165d877c68f98d6ea02f1a373bed7d027b42ca714f33f1baa73b67c8ce6215ab572d1820b72ee8e7a96c6d914927409b4a22241ce77bc3a'
